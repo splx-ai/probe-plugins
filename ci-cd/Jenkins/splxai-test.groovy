1861b17b8483b4d3f8e5ee6c73946f928623b143
Mateja Vuradin <mateja.vuradin@gmail.com>
2026-01-08 09:57:06 +0100

    fix: jenkins

--- ci-cd/Jenkins/splxai-test.groovy
+++ ci-cd/Jenkins/splxai-test.groovy
@@ -7,6 +7,7 @@
 //
 // The following values should be added as environment variables.
 //    SPLXAI_TARGET_ID: the Target ID of the pre defined Target, from SplxAI Probe, Target Settings
+//    SPLXAI_WORKSPACE_ID: the Workspace ID of the pre defined Target, from SplxAI Probe (can be found in the URL when viewing the Target - e.g 163 in https://probe.splx.ai/w/163/target/000)
 //    SPLXAI_API_KEY: the API key generated from SplxAI Probe, Account Settings
 //    SPLXAI_PLATFORM_URL: the SplxAI url that you login to (e.g https://probe.splx.ai/ or https://us.probe.splx.ai/)
 //    SPLXAI_API_URL: the SplxAI url that you login to (e.g https://api.probe.splx.ai/ or https://api.us.probe.splx.ai/)
@@ -20,6 +21,7 @@ pipeline {
   
   environment {
     SPLXAI_TARGET_ID = 000
+    SPLXAI_WORKSPACE_ID = 000
     SPLXAI_PLATFORM_URL = "https://probe.splx.ai"
     SPLXAI_API_URL = "https://api.probe.splx.ai"
    }
@@ -43,7 +45,7 @@ pipeline {
               )
     
               RESPONSE=$(curl -s --request POST \
-                --url $SPLXAI_API_URL/api/v2/test-run/trigger \
+                --url $SPLXAI_API_URL/api/workspaces/$SPLXAI_WORKSPACE_ID/test-run/trigger \
                 --header 'Content-Type: application/json' \
                 --header "X-Api-Key: $SPLXAI_API_KEY" \
                 --data "$REQUEST_BODY")
@@ -57,7 +59,7 @@ pipeline {
               
               echo "###### Triggered SplxAI Test Run #######"
               echo "=> Click on the link below to see the Test Run results"
-              echo "=> https://probe.splx.ai/target/$SPLXAI_TARGET_ID/test-run-history/$TEST_RUN_ID"
+              echo "=> https://probe.splx.ai/w/$SPLXAI_WORKSPACE_ID/target/$SPLXAI_TARGET_ID/test-runs/$TEST_RUN_ID"
            '''
             }
         }
